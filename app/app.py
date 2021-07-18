e563b7df74f11717390f9c8355641d808ab16a0e
Vishwas Srivastava <srivastavavishwas92@gmail.com>
2021-07-19 03:34:07 +0530

    tfidfvectorizer, sentiment model used

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ Created on Sat Jul 17 19:15:32 2021
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 import json
@@ -31,14 +31,16 @@ vectorizer = pickle.load(open(vectorizer_file, 'rb'))
 
 def get_data():
     data = pd.read_csv(data_file)
+    # indices = pd.Series(data.index, index=data['reviews_username'])
+    # all_users = [data['reviews_username'][i] for i in range(len(data['reviews_username']))]
     return data
 
 
 def create_sim():
-    data = get_data()
+    data  = get_data()
     # creating a count matrix
-    cv = CountVectorizer()
-    print(data['clean_text'])
+    cv = TfidfVectorizer(stop_words='english')
+    # print(data['clean_text'])
     data = data[~(data['clean_text'].isnull())]
     print(data['clean_text'].isna().sum())
     count_matrix = cv.fit_transform(data['clean_text'])
@@ -48,35 +50,56 @@ def create_sim():
 
 
 def get_reviews(product_name):
-    data = get_data()
+    data  = get_data()
     reviews = data[data['name'] == product_name]['reviews_text']
     return reviews
 
 
-def get_recommendations(user_name):
+def get_recommendations(user_name, data):
     # user_name = user_name.lower()
-    data = get_data()
+    # data = get_data()
+    # try:
+    #     data, sim = create_sim()
+    #     data.head()
+    #     sim.shape
+    # except Exception as ex:
+    #     print("Exception occured")
+    #     raise ex
     try:
-        data, sim = create_sim()
         data.head()
-        sim.shape
-    except Exception as ex:
-        print("Exception occured")
-        raise ex
+        similarity.shape
+    except:
+        data, similarity = create_sim()
 
     if user_name not in data['reviews_username'].unique():
         return (
             'Sorry! The user you searched is not in our database. Please check the spelling or try with some other user')
     else:
-        i = data.loc[data['reviews_username'] == user_name].index[0]
-        lst = list(enumerate(sim[i]))
-        lst = sorted(lst, key=lambda x: x[1], reverse=True)
-        lst = lst[1:11]
-        l = []
-        for i in range(len(lst)):
-            a = lst[i][0]
-            l.append(data['name'][a])
-        return l
+        # indices =
+        idx  = data.loc[data['reviews_username'] == user_name].index[0]
+        # print(len(idx))
+        sim_scores = list(enumerate(similarity[idx]))
+        # print(sim_scores)
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        # sim_scores = sim_scores[1:11]
+        product_indices = [i[0] for i in sim_scores]
+        products_name = data['name'].iloc[product_indices]
+        products_brand = data['brand'].iloc[product_indices]
+        manufacturers = data['manufacturer'].iloc[product_indices]
+        return_df = pd.DataFrame(columns=['Product', 'Brand', 'Manufacturer'])
+        return_df['Product'] = products_name
+        return_df['Brand'] = products_brand
+        return_df['Manufacturer'] = manufacturers
+        return return_df
+        # i = data.loc[data['reviews_username'] == user_name].index[0]
+        # lst = list(enumerate(similarity[i]))
+        # lst = sorted(lst, key=lambda x: x[1], reverse=True)
+        # lst = lst[1:11]
+        # l = []
+        # for i in range(len(lst)):
+        #     a = lst[i][0]
+        #     l.append(data['name'][a])
+        # return l
 
 
 def get_suggestions():
@@ -102,29 +125,49 @@ def recommend():
 
     # user = user.upper()
     # 2. Get top 5 recommendations
-    product_recommendations = get_recommendations(user)
-    print(product_recommendations)
-    print(type(product_recommendations))
+    data = get_data()
+    product_recommendations = get_recommendations(user, data)
+    # print(product_recommendations)
+    # print(type(product_recommendations))
     if isinstance(product_recommendations, str):  # no such user found in the database
         suggestions = get_suggestions()
         print("Got suggestions : {}".format((suggestions)))
         return render_template('home.html', user_not_found=True, suggestions=suggestions)
     else:
 
-        data = get_data()
+        # data = get_data()
         product_cards = []
-        product_recommendations = set(product_recommendations)
-        for product in product_recommendations:
-            print(product)
-            name = product
-            # manufacturer = data[data['name'] == product['name']]['manufacturer']
-            # brand = data[data['name'] == product['name']]['brand']
+        product_added = set()
+        for i in range(len(product_recommendations)):
+            if product_recommendations.Product.iloc[i] not in product_added:
 
-            product_card = {'product_name': name, 'manufacturer': "dummy", 'brand': "dummy"}
-            product_cards.append(product_card)
+                product_added.add(product_recommendations.Product.iloc[i])
+                product_reviews = get_reviews(product_recommendations.Product.iloc[i])
+                product_reviews_vector = vectorizer.transform(product_reviews)
+                pred = clf.predict(product_reviews_vector)
+                print(pred)
+                reviews_status = ('Good' if 1 in pred else 'Bad')
+                product_card = {
+                    'product_name': product_recommendations.Product.iloc[i],
+                    'manufacturer': product_recommendations.Manufacturer.iloc[i],
+                    'brand': product_recommendations.Brand.iloc[i],
+                    'sentiment': reviews_status
+                    }
+                product_cards.append(product_card)
+
+        product_cards = product_cards[0:6]
+        # product_recommendations = set(product_recommendations)
+        # for product in product_recommendations:
+        #     print(product)
+        #     name = product
+        #     # manufacturer = data[data['name'] == product['name']]['manufacturer']
+        #     # brand = data[data['name'] == product['name']]['brand']
+        #
+        #     product_card = {'product_name': name, 'manufacturer': "dummy", 'brand': "dummy"}
+        #     product_cards.append(product_card)
 
         # get user names for auto completion
-        suggestions = get_suggestions()
+        # suggestions = get_suggestions()
 
         return render_template('recommend.html', product_cards=product_cards)
 
